9bf07c13bf2e8e6436af5d649cb5275cb07a09ae
ksugaevskaya <ksu.gaevskaya@gmail.com>
2026-03-25 22:30:40 +0100

    feat: initial structure for event listeners

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,35 @@
-import { WebSocketServer } from 'ws';
-
+import { WebSocketServer } from "ws";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // WebSocket server
 const wss = new WebSocketServer({ port: PORT });
+
+wss.on("connection", (ws, request) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    console.log("Received:", message.toString());
+
+    ws.send(
+      JSON.stringify({
+        type: "reg",
+        data: {
+          name: "Ksu",
+          index: 0,
+          error: false,
+          errorText: "",
+        },
+        id: 0,
+      }),
+    );
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+
+  ws.on("error", (error) => {
+    console.error("WebSocket error:", error);
+  });
+});
