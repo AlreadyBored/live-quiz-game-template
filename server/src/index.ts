206b9790c7aa1603c9d042c159924ad11a929b53
Абрамов Алексей <x3m.f@mail.ru>
2026-03-25 22:41:21 +0400

    init project

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,20 @@
 import { WebSocketServer } from 'ws';
 
-
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // WebSocket server
 const wss = new WebSocketServer({ port: PORT });
+
+wss.on('connection', (ws) => {
+  console.log('Connected');
+
+  ws.on('message', (msg) => {
+    console.log('Message:', msg.toString());
+  });
+
+  ws.on('close', () => {
+    console.log('Close client');
+  });
+});
+
+console.log(`WebSocket server running on ws://localhost:${PORT}`);
