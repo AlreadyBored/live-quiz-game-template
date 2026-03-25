758e71cead10fb47d4cfe010d74d9144fc60098c
Viktoriia Petukhova <viktoriiap0205199@gmail.com>
2026-03-25 19:04:34 +0100

    feat: add functionality for creating the new game and structure for future fuctiones

--- server/src/handlers/messageHandler.ts
+++ server/src/handlers/messageHandler.ts
@@ -1,7 +1,11 @@
 const players = new Map<string, { name: string; index: string; score: number }>();
+const games = new Map<string, any>();
 
 function generateId() {
   return Math.random().toString(36).substring(2, 10);
+}
+function generateCode() {
+  return Math.random().toString(36).substring(2, 8).toUpperCase();
 }
 
 const socketToPlayer = new Map<any, string>();
@@ -37,4 +41,59 @@ export function handleMessage(ws: any, message: string) {
     );
 
   }
+  if(type === 'game_created') {
+    const playerId = socketToPlayer.get(ws);
+     if (!playerId) return;
+    const { questions } = data;
+
+    const gameId = generateId();
+    const code = generateCode();
+
+    const game = {
+    id: gameId,
+    code,
+    hostId: playerId,
+    questions,
+    players: [],
+    currentQuestion: -1,
+    status: 'waiting',
+  };
+ games.set(gameId, game);
+
+   ws.send(
+    JSON.stringify({
+      type: 'game_created',
+      data: {
+        gameId,
+        code,
+      },
+      id: 0,
+    })
+  );
+  }
+    if(type === 'game_joined') {
+    
+  }
+    if(type === 'player_joined') {
+    
+  }
+    if(type === 'update_players') {
+    
+  }
+    if(type === 'game_created') {
+    
+  }
+    if(type === 'answer_accepted') {
+    
+  }
+    if(type === 'question_result') {
+    
+  }
+    if(type === 'game_finished') {
+    
+  }
+    if(type === 'error') {
+    
+  }
+
 }
