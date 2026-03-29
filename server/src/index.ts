3f4c61ca0d4da97190ffe6d0d6aa9d0ddc70a4ab
Hanna <hannamuzychuk@gmail.com>
2026-03-29 17:00:33 +0200

    add memory storage

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,5 +3,5 @@ import { WebSocketServer } from 'ws';
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
-// WebSocket server
 const wss = new WebSocketServer({ port: PORT });
+console.log(`WebSocket server running on ws://localhost:${PORT}`);
