9ed1b2afe128a4f837db5b8da66c677dcbbe4f23
Tomasz Lipowiec <t.lipowiec@lemonmind.com>
2026-03-29 22:51:30 +0200

    feat: getting port from dotenv and informing about it at start

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,27 @@
+import 'dotenv/config';
 import { WebSocketServer } from 'ws';
-
+import type { WSMessage, RegData } from './types.js';
+import { handleReg } from './handlers/reg.js';
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
-// WebSocket server
-const wss = new WebSocketServer({ port: PORT });
+const wss = new WebSocketServer({ port: PORT }, () => {
+  console.log(`WebSocket server is running on ws://localhost:${PORT}`);
+});
+
+wss.on('connection', (ws) => {
+  ws.on('message', (raw: Buffer) => {
+    let msg: WSMessage;
+    try {
+      msg = JSON.parse(raw.toString());
+    } catch {
+      return;
+    }
+
+    switch (msg.type) {
+      case 'reg':
+        handleReg(ws, msg.data as RegData);
+        break;
+    }
+  });
+});
