71eb93888bb4e9e8bd970cb3cea94be23cb92741
Timofey Naryshkin <naryshkint4@gmail.com>
2026-03-29 13:59:22 +0900

    feat: handle game creation

--- server/src/db/db.ts
+++ server/src/db/db.ts
@@ -1,4 +1,6 @@
-import { Player } from "../types";
+import { Game, Player } from "../types";
 import type { WebSocket } from "ws";
 
 export const players = new Map<WebSocket, Player>();
+
+export const games = new Map<string, Game>()
--- server/src/utils/messageHandler.ts
+++ server/src/utils/messageHandler.ts
@@ -1,27 +1,31 @@
 import { RawData } from "ws";
 import { isMessage } from "./isMessage";
-import { Player, RegData, WSMessage } from "../types";
+import { CreateGameData, Game, Player, RegData, WSMessage } from "../types";
 import { randomUUID } from "node:crypto";
-import { players } from "../db/db";
+import { games, players } from "../db/db";
 import type { WebSocket } from "ws";
+import { generateCode } from "./generateCode";
 
 export function messageHandler(ws: WebSocket, data: RawData) {
   const message = JSON.parse(data.toString());
   if (!isMessage(message)) return;
+
+  const response: WSMessage = {
+    type: "",
+    data: null,
+    id: 0,
+  };
 
   switch (message.type) {
     case "reg": {
       const data = message.data as RegData;
       const player = players.get(ws);
 
-      const response: WSMessage = {
-        type: "reg",
-        data: {
-          name: data.name,
-          error: false,
-          errorText: "",
-        },
-        id: 0,
+      response.type = "reg";
+      response.data = {
+        name: data.name,
+        error: false,
+        errorText: "",
       };
 
       if (player) {
@@ -36,12 +40,34 @@ export function messageHandler(ws: WebSocket, data: RawData) {
         players.set(ws, newPlayer);
         response.data.index = newPlayer.index;
       }
-
-      ws.send(JSON.stringify(response));
       break;
     }
-    
+    case "create_game": {
+      const data = message.data as CreateGameData;
+      const host = players.get(ws);
+      if (!host) return;
+      const game: Game = {
+        id: randomUUID(),
+        code: generateCode(),
+        hostId: host.index,
+        questions: data.questions,
+        players: [],
+        currentQuestion: 0,
+        status: "waiting",
+        playerAnswers: new Map(),
+      };
+      games.set(game.id, game);
+
+      response.type = "game_created";
+      response.data = {
+        gameId: game.id,
+        code: game.code,
+      };
+      break;
+    }
     default:
       break;
   }
+
+  ws.send(JSON.stringify(response));
 }
