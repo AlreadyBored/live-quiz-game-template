08e0a4126092f0bd66a600a8db993b75d0923cc8
Timofey Naryshkin <naryshkint4@gmail.com>
2026-03-29 12:55:12 +0900

    feat: install deps, log wss adress on start, add simple wss handler

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,20 @@
-import { WebSocketServer } from 'ws';
-
+import { WebSocketServer } from "ws";
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 
 // WebSocket server
 const wss = new WebSocketServer({ port: PORT });
+const address = wss.address();
+if (address && typeof address === "object") {
+  console.log(
+    `Started WebSocket server on address: ${address.address}, port: ${address.port}`,
+  );
+}
+
+wss.on("connection", (ws) => {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+  });
+});
